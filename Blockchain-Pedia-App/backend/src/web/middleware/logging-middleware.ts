544e270b6ec823be855313ce330b8f86a9d515ba
Mihai Cristian-Laurentiu <mihai.laurentiu24@yahoo.com>
2022-11-22 22:05:33 +0200

    Revert "Added log data for different filters"
    This reverts commit eb25f929c9a2b3355062cd91610a934003b6be00.

--- Blockchain-Pedia-App/backend/src/web/middleware/logging-middleware.ts
+++ Blockchain-Pedia-App/backend/src/web/middleware/logging-middleware.ts
@@ -1,21 +1,10 @@
 import {NextFunction, Request, Response} from "express";
 
 export const logRequestMiddleware = (req: Request<any>, res: Response, next: NextFunction) => {
-    const fs = require('fs');
-    const util = require('util');
-    const log_file = fs.createWriteStream('request_log.txt', {flags: 'a'});
-    const log_stdout = process.stdout;
-    console.log = function (s) {
-        log_file.write(util.format('%s', s));
-        log_stdout.write(util.format('%s', s));
-    }
-    console.log(`REQUEST ${req.method} ${req.url}\n`)
-    console.log("Body\n")
-    console.log(req.body);
-    console.log("\nHeaders\n");
-    console.log(req.body);
-    console.log("\nQuery\n");
-    console.log(req.query);
-    console.log("\n\n")
+    console.log("Request Body: ", req.body);
+    console.log("Request Headers: ", req.body);
+    console.log("Request Query: ", req.query);
+    console.log("Request URL: ", req.url);
+
     next();
 }
