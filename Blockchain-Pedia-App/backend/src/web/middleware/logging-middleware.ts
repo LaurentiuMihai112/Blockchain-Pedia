eb25f929c9a2b3355062cd91610a934003b6be00
Mihai Cristian-Laurentiu <mihai.laurentiu24@yahoo.com>
2022-11-22 15:09:30 +0200

    Added log data for different filters

--- Blockchain-Pedia-App/backend/src/web/middleware/logging-middleware.ts
+++ Blockchain-Pedia-App/backend/src/web/middleware/logging-middleware.ts
@@ -1,10 +1,21 @@
 import {NextFunction, Request, Response} from "express";
 
 export const logRequestMiddleware = (req: Request<any>, res: Response, next: NextFunction) => {
-    console.log("Request Body: ", req.body);
-    console.log("Request Headers: ", req.body);
-    console.log("Request Query: ", req.query);
-    console.log("Request URL: ", req.url);
-
+    const fs = require('fs');
+    const util = require('util');
+    const log_file = fs.createWriteStream('request_log.txt', {flags: 'a'});
+    const log_stdout = process.stdout;
+    console.log = function (s) {
+        log_file.write(util.format('%s', s));
+        log_stdout.write(util.format('%s', s));
+    }
+    console.log(`REQUEST ${req.method} ${req.url}\n`)
+    console.log("Body\n")
+    console.log(req.body);
+    console.log("\nHeaders\n");
+    console.log(req.body);
+    console.log("\nQuery\n");
+    console.log(req.query);
+    console.log("\n\n")
     next();
 }
