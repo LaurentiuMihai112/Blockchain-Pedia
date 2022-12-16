c3ec987dfb8e4e61966748b6c4835da5c0b48358
Mihai Cristian-Laurentiu <mihai.laurentiu24@yahoo.com>
2022-12-16 14:30:33 +0200

    Code cleanup

--- Blockchain-Pedia-App/frontend/src/app/app.module.ts
+++ Blockchain-Pedia-App/frontend/src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { DataViewComponent } from './data-view/data-view.component';
-import { FilterComponent } from './filter/filter.component';
-import { ChartComponent } from './chart/chart.component';
-import { BestFitComponent } from './best-fit/best-fit.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {HomeComponent} from './home/home.component';
+import {DataViewComponent} from './data-view/data-view.component';
+import {FilterComponent} from './filter/filter.component';
+import {ChartComponent} from './chart/chart.component';
+import {BestFitComponent} from './best-fit/best-fit.component';
 
 @NgModule({
   declarations: [
@@ -29,4 +29,5 @@ import { BestFitComponent } from './best-fit/best-fit.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- Blockchain-Pedia-App/frontend/src/app/best-fit/best-fit.component.spec.ts
+++ Blockchain-Pedia-App/frontend/src/app/best-fit/best-fit.component.spec.ts
@@ -1,6 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { BestFitComponent } from './best-fit.component';
+import {BestFitComponent} from './best-fit.component';
 
 describe('BestFitComponent', () => {
   let component: BestFitComponent;
@@ -8,9 +8,9 @@ describe('BestFitComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BestFitComponent ]
+      declarations: [BestFitComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(BestFitComponent);
     component = fixture.componentInstance;
